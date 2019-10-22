f06e4794c4b160ee9cd42b4091beecd1c6645ccd
Dmitrii <dshitikov@rtech.ru>
2019-10-22 22:24:05 +0300

    l3 p2 View brands

--- app/controllers/MainController.php
+++ app/controllers/MainController.php
@@ -12,6 +12,8 @@ class MainController extends AppController
 {
     public function indexAction()
     {
+        $brands = R::find('brand', 'LIMIT 3');
+        $this->set(['brands' => $brands]);
         $this->setMeta(App::$app->getProperty('shop_name'), 'самая главная', 'круто супер главная');
     }
 }
