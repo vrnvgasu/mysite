bd665eed8ab43cdb54717c4347a61e3497df7a0a
Dmitrii <dshitikov@rtech.ru>
2019-12-09 19:05:36 +0300

    l3 p3 Admin dashboard

--- app/controllers/admin/MainController.php
+++ app/controllers/admin/MainController.php
@@ -4,10 +4,18 @@
 namespace app\controllers\admin;
 
 
+use RedBeanPHP\R;
+
 class MainController extends AppController
 {
     public function indexAction()
     {
+        $countNewOrders = R::count('order', "status = '0'");
+        $countUsers = R::count('user');
+        $countProducts = R::count('product');
+        $countCategories = R::count('category');
+
         $this->setMeta('Панель управления');
+        $this->set(compact('countNewOrders', 'countCategories', 'countUsers', 'countProducts'));
     }
 }
